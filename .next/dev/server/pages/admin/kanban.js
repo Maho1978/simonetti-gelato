329398d53d7b30b9ba9122ab322b6b57911f09c3
Maho1978 <mahmutduran@hotmail.de>
2026-03-16 19:28:31 +0100

    fix: gitignore repariert, .next entfernt, account + features aktualisiert

--- .next/dev/server/pages/admin/kanban.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../chunks/ssr/[turbopack]_runtime.js")("server/pages/admin/kanban.js")
-R.c("server/chunks/ssr/dbe97_f10d9e5d._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__85f8da2a._.js")
-R.c("server/chunks/ssr/dbe97_e7f95d29._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__fd58ed4c._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__4d769fb5._.js")
-R.m("[project]/OneDrive/Desktop/simonetti-hybrid/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/OneDrive/Desktop/simonetti-hybrid/pages/admin/kanban.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/OneDrive/Desktop/simonetti-hybrid/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/OneDrive/Desktop/simonetti-hybrid/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/OneDrive/Desktop/simonetti-hybrid/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/OneDrive/Desktop/simonetti-hybrid/pages/admin/kanban.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/OneDrive/Desktop/simonetti-hybrid/pages/_document.tsx [ssr] (ecmascript)\", INNER_APP => \"[project]/OneDrive/Desktop/simonetti-hybrid/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
